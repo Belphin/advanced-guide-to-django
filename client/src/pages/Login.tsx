a52615d3103f9ad6f61658e010803651bafca8e3
Belphin <findolph0003@gmail.com>
2023-10-04 12:34:15 +0300

    Add auth pages layout

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button, Card, Input } from "antd";
+import { Container, FormGroup, Form, FormLabel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { RouteNames } from "router";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Container className="d-flex justify-content-center align-items-center">
+      <Card title="Login Form" type="inner" style={{ width: "440px" }}>
+        <Form>
+          <FormGroup className="mb-3">
+            <FormLabel>Email</FormLabel>
+            <Input />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Password</FormLabel>
+            <Input.Password
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </FormGroup>
+          <div className="mt-4 d-flex justify-content-between">
+            <div className="d-flex align-items-center">
+              <div style={{ marginRight: "4px" }}>No account?</div>
+              <Link to={RouteNames.REGISTER}>Register</Link>
+            </div>
+            <Button type="primary">Login</Button>
+          </div>
+        </Form>
+      </Card>
+    </Container>
+  );
 };
 
 export default Login;
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button, Card, Input, Select } from "antd";
+import { Container, FormGroup, Form, FormLabel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { RouteNames } from "router";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Container className="d-flex justify-content-center align-items-center">
+      <Card title="Register Form" type="inner" style={{ width: "440px" }}>
+        <Form>
+          <FormGroup className="mb-3">
+            <FormLabel>Email *</FormLabel>
+            <Input type="text" />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <FormLabel>First name</FormLabel>
+            <Input type="text" />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <FormLabel>Last name</FormLabel>
+            <Input type="text" />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <FormLabel>Role *</FormLabel>
+            <Select
+              style={{ display: "block" }}
+              options={[{ value: 0, label: "Blogger" }]}
+            />
+          </FormGroup>
+          <FormGroup className="mb-3">
+            <FormLabel>Password *</FormLabel>
+            <Input.Password
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Confirm Password *</FormLabel>
+            <Input.Password
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </FormGroup>
+          <div className="mt-4 d-flex justify-content-between">
+            <div className="d-flex align-items-center">
+              <div style={{ marginRight: "4px" }}>Have an account?</div>
+              <Link to={RouteNames.LOGIN}>Log in</Link>
+            </div>
+            <Button type="primary">Register</Button>
+          </div>
+        </Form>
+      </Card>
+    </Container>
+  );
 };
 
 export default Register;
