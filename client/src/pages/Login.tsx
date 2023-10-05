28a40ed7ccecff70e79bb6bad9ef48bbaaa43f74
Belphin <findolph0003@gmail.com>
2023-10-05 12:48:11 +0300

    Add react-hook-form

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,22 +4,51 @@ import { Button, Card, Input } from "antd";
 import { Container, FormGroup, Form, FormLabel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { RouteNames } from "router";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+interface FormData {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center">
       <Card title="Login Form" type="inner" style={{ width: "440px" }}>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <FormGroup className="mb-3">
             <FormLabel>Email</FormLabel>
-            <Input />
+            <Controller
+              name="email"
+              control={control}
+              rules={{ required: "This field is required" }}
+              render={({ field }) => <Input type="text" {...field} />}
+            />
           </FormGroup>
           <FormGroup>
             <FormLabel>Password</FormLabel>
-            <Input.Password
-              iconRender={(visible) =>
-                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-              }
+            <Controller
+              name="password"
+              control={control}
+              rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Input.Password
+                  iconRender={(visible) =>
+                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                  }
+                  {...field}
+                />
+              )}
             />
           </FormGroup>
           <div className="mt-4 d-flex justify-content-between">
@@ -27,7 +56,9 @@ const Login = () => {
               <div style={{ marginRight: "4px" }}>No account?</div>
               <Link to={RouteNames.REGISTER}>Register</Link>
             </div>
-            <Button type="primary">Login</Button>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
           </div>
         </Form>
       </Card>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -4,45 +4,104 @@ import { Button, Card, Input, Select } from "antd";
 import { Container, FormGroup, Form, FormLabel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { RouteNames } from "router";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+interface FormData {
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: number;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center">
       <Card title="Register Form" type="inner" style={{ width: "440px" }}>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <FormGroup className="mb-3">
             <FormLabel>Email *</FormLabel>
-            <Input type="text" />
+            <Controller
+              name="email"
+              control={control}
+              rules={{ required: "This field is required" }}
+              render={({ field }) => <Input type="text" {...field} />}
+            />
           </FormGroup>
           <FormGroup className="mb-3">
             <FormLabel>First name</FormLabel>
-            <Input type="text" />
+            <Controller
+              name="firstName"
+              control={control}
+              // rules={{ required: "This field is required" }}
+              render={({ field }) => <Input type="text" {...field} />}
+            />
           </FormGroup>
           <FormGroup className="mb-3">
             <FormLabel>Last name</FormLabel>
-            <Input type="text" />
+            <Controller
+              name="lastName"
+              control={control}
+              // rules={{ required: "This field is required" }}
+              render={({ field }) => <Input type="text" {...field} />}
+            />
           </FormGroup>
           <FormGroup className="mb-3">
             <FormLabel>Role *</FormLabel>
-            <Select
-              style={{ display: "block" }}
-              options={[{ value: 0, label: "Blogger" }]}
+            <Controller
+              name="role"
+              control={control}
+              // rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  style={{ display: "block" }}
+                  options={[{ value: 0, label: "Blogger" }]}
+                />
+              )}
             />
           </FormGroup>
           <FormGroup className="mb-3">
             <FormLabel>Password *</FormLabel>
-            <Input.Password
-              iconRender={(visible) =>
-                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-              }
+            <Controller
+              name="password"
+              control={control}
+              // rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Input.Password
+                  {...field}
+                  iconRender={(visible) =>
+                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                  }
+                />
+              )}
             />
           </FormGroup>
           <FormGroup>
             <FormLabel>Confirm Password *</FormLabel>
-            <Input.Password
-              iconRender={(visible) =>
-                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-              }
+            <Controller
+              name="confirmPassword"
+              control={control}
+              // rules={{ required: "This field is required" }}
+              render={({ field }) => (
+                <Input.Password
+                  {...field}
+                  iconRender={(visible) =>
+                    visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                  }
+                />
+              )}
             />
           </FormGroup>
           <div className="mt-4 d-flex justify-content-between">
@@ -50,7 +109,9 @@ const Register = () => {
               <div style={{ marginRight: "4px" }}>Have an account?</div>
               <Link to={RouteNames.LOGIN}>Log in</Link>
             </div>
-            <Button type="primary">Register</Button>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
           </div>
         </Form>
       </Card>
